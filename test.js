3864ebbc1b3fd0c9e2444c94c253b0607b38fb93
Karan2896 <karanmakwana@worldteam11.com>
2019-11-08 17:00:44 +0530

    new file

--- test.js
+++ test.js
@@ -1,22 +1,7 @@
-//var dealCards = require('./GameScripts/DealCards');
+var io = require('socket.io')(process.env.PORT || 80);
 
-// var d_card = require('./GameScripts/DealCards');
-
-// var handeval = require('./GameScripts/HandEvaluator');
-// var dd_card = new d_card();
-
-// const cardClass = require('./GameScripts/Card');
-
-// const cardclassobj = new cardClass(); 
-
-
-// console.log(cardclassobj.GET_CARD.VALUE.EIGHT);
-
-//var bb = new dealCards();
-// var k = new d_card(4);
-// k.Deal();
-//bb.setUpDeck();
-//bb.Deal();
-// dd_card.getHandsNew();
-
-
+io.on('connection', function (socket) {
+    console.log('-------Connection made-------');
+	
+	
+});
